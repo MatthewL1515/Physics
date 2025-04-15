638d162851c3736e7ed6443408dc152ca2599dea
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-04-15 13:41:17 -0400

    fixing typos

--- Physics_Vector/sketch.js
+++ Physics_Vector/sketch.js
@@ -19,6 +19,7 @@ class Mover { // noun
   }
   
   update() {
+    this.applyForce(createVector(wind,G))
     this.velocity.add(this.acceleration)
     this.move()
     this.containWithinWindow()
@@ -42,20 +43,54 @@ class Mover { // noun
   containWithinWindow() {
     if( this.position.x < this.r ) { // moved off the left hand side
       this.position.x = this.r
-      this.velocity.mult(-1,1)
+      this.velocity.x *= -0.9
     }
     if( this.position.x > width - this.r ) { // right
       this.position.x = width-this.r
-      this.velocity.mult(-1,1)
+      this.velocity.x *= -0.9
     }
     if( this.position.y < this.r ) { // top
       this.position.y = this.r
-      this.velocity.mult(1,-1)
+      this.velocity.y *= -0.9
     }
     if( this.position.y > height - this.r ) { // bottom
       this.position.y = height-this.r
-      this.velocity.mult(1,-1)
+      this.velocity.y *= -0.9
     }    
       
   }
+  
+  checkCollision(other) {
+    let diff = p5.Vector.sub(other.position, this.position)
+    let distance = diff.mag()
+    let minDist = this.r + other.r
+
+    if (distance < minDist) {
+      // Push circles apart slightly to avoid stacking together
+      let overlap = (minDist - distance) / 2
+      let correction = diff.copy().setMag(overlap)
+      this.position.sub(correction)
+      other.position.add(correction)
+
+      // Decide which axis the collision happened on
+      if (abs(diff.x) > abs(diff.y)) {
+        // x axis collision, bounce horizontally
+        this.velocity.x *= -0.9
+        other.velocity.x *= -0.9
+      } else {
+        // y axis collision, bounce vertically
+        this.velocity.y *= -0.9
+        other.velocity.y *= -0.9
+      }
+    }
+  }
+  e
+  attractTo(hole) {
+    let force = createVector(hole.x, hole.y).sub(this.position)
+    let distanceSq = constrain(force.magSq(), 100, 10000)
+    let strength = hole.strength / distanceSq
+    force.setMag(strength)
+    this.applyForce(force)
+  }
+
 }
